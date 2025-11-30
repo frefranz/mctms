039861ded1d90eeb110f1ca9f24145ff1fe66d1e
Franz <franz.f@web.de>
2025-11-30 03:20:33 +0100

    eval project blinky added for an arduino nano

---
+++ eval_projects/blinky_arduino_nano/src/main.cpp
@@ -0,0 +1,27 @@
+
+// ***********************************************************
+// *                                                         *
+// * Laesst auf einem Arduino Uno eine an Pin 13 und GND an- *
+// * geschlossene LED blinken                     2025-11-29 *
+// *                                                         *
+// ***********************************************************
+
+// My Arduino Uno Board: eine gelbe LED auf dem Board blinkt mit,
+// offensichtlich ebenfalls an Pin 13 angeschlossen
+
+#include <Arduino.h>
+
+int ledPin1 = 13; // LED an digitalen Pin 13 angeschlossen
+
+void setup()
+{
+  pinMode(ledPin1, OUTPUT); // setze digitalen Pin als Output
+}
+
+void loop()
+{
+  digitalWrite(ledPin1, HIGH);
+  delay(700);
+  digitalWrite(ledPin1, LOW);
+  delay(700);
+}
